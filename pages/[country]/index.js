d0634cf27089687b03171310d756fea20eb698cd
Lucas Lacava <lucas.lacava@gmail.com>
2020-06-13 18:36:31 -0300

    update fetch links

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -48,7 +48,7 @@ Home.getInitialProps = async (context) => {
   try {
     const country = context.query.country || "us";
     const response = await axios.get(
-      `http://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
+      `https://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
     );
 
     return {
